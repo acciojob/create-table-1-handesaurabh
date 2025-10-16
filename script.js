06a0fb3738c90c6b88f8d62bdec7601601dbbe4b
handesaurabh <handesaurabh2004@gmail.com>
2025-10-16 12:19:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function insert_Row() {
     //Write your code here
-  
+  const tb = document.getElementById("sampleTable");
+    const newRow = tb.insertRow(0);
+    const cell1 = newRow.insertCell(0);
+    cell1.innerHTML = "New Cell1";
+    const cell2 = newRow.insertCell(1);
+    cell2.innerHTML = "New Cell2";
   
 }
